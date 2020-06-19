f462c1e8bbabe63f8be244dc45c99fa3a0f9b3f4
Muhammed Saifudeen Salaudeen <mr.salaudeen.official@gmail.com>
2020-06-19 12:52:24 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import env from './config/env';
-// import { AppConfigService } from './config/app/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(env.port);
 }
 
 bootstrap();
