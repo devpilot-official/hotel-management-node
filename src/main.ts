2fcb9948d1ead3aef887cb43e76a6d34fdc7d33f
Muhammed Saifudeen Salaudeen <mr.salaudeen.official@gmail.com>
2020-06-19 10:04:48 +0100

    chore: added env variables and connected to mongodb, also added the auth create user

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,11 @@ import env from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
 
   app.enableCors();
+
   await app.listen(env.port);
-=======
   await app.listen(process.env.PORT || 3001);
->>>>>>> f2b893071bf7c60464ad1982d669344fda49b320
 }
+
 bootstrap();
